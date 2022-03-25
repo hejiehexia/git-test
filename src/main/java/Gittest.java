616badee205192c6bfce8f2aa2fb8948e87ef1d8
hejie <hejie@hj.com>
2022-03-25 11:11:54 +0800

    hot-fix commit

--- src/main/java/Gittest.java
+++ src/main/java/Gittest.java
@@ -3,5 +3,6 @@ public class Gittest {
         System.out.println("你好git");
         System.out.println("你好git2");
         System.out.println("你好git3");
+        System.out.println("你好git4！");
     }
 }
